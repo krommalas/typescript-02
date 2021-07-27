1066c3e98c98327003893860534daf8d7bb09694
krommalas <krommalas@gmail.com>
2021-07-28 00:33:32 +0700

    class : Observer , Function : Interval , Function of , Function from

--- index.ts
+++ index.ts
@@ -1,6 +1,57 @@
-// Import stylesheets
-import './style.css';
+console.clear();
+//OBJ : Type = Observer ส่งเข้า Fucntion
+interface Observer {
+  next: (value: any) => void;
+  error: (err: any) => void;
+  complete: () => void;
+}
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+type TearDown = () => void;
+
+//OBJ
+const observer: Observer = {
+  next: (value: any) => console.log('next :', value),
+  error: (err: any) => console.log('error', err),
+  complete: () => console.log('complete')
+};
+
+class Observable {
+  subscriber: (observer: Observer) => TearDown;
+  constructor(subscriber: (observer: Observable) => TearDown) {
+    this.subscriber = subscriber;
+  }
+  subscribe(observer: Observer) {
+    const teardown: TearDown = this.subscriber(observer);
+    return { unsubscribe: () => teardown() };
+  }
+}
+function Interval(millisec: number) {
+  return new Observable(observer => {
+    let i = 0;
+    const index = setInterval(() => observer.next(i++), millisec);
+    const teardown = () => clearInterval(index);
+    return teardown;
+  });
+}
+
+function of(...dataList: any[]) {
+  return new Observable(observer => {
+    dataList.forEach(data => observer.next(data));
+    observer.complete();
+    return () => {};
+  });
+}
+
+function from(dataList: any[]) {
+  return new Observable(observer => {
+    dataList.forEach(data => observer.next(data));
+    observer.next();
+    return () => {};
+  });
+}
+
+//const source = Interval(1000);
+//const source = of(10,20,30,40,50,"60");
+const source = from(["banana",'apple',50,]);
+const subscription = source.subscribe(observer);
+setTimeout(() => subscription.unsubscribe(), 5000);
